b52624e0fd6b52a9227599da99ee3d1547736dd8
Dmitriy Golubev <122462302+Lirikman@users.noreply.github.com>
2026-06-28 07:23:35 +0000

    added checks for borderline cases

--- cmd/main.go
+++ cmd/main.go
@@ -11,4 +11,7 @@ func main() {
 	fmt.Println(code.GeneratePassword(12, 123, true, true, false))
 	fmt.Println(code.GeneratePassword(12, 123, true, true, true))
 	fmt.Println(code.GeneratePassword(8, 1, false, false, false))
+	fmt.Println(code.GeneratePassword(-3, 42, true, true, false))
+	fmt.Println(code.GeneratePassword(8, 0, true, true, false))
+	fmt.Println(code.GeneratePassword(8, -42, true, true, false))
 }
--- password_generator.go
+++ password_generator.go
@@ -10,9 +10,21 @@ func NextRandom(number int) int {
 }
 
 func GeneratePassword(length, seed int, useUppercase, useDigits, useSpecial bool) string {
+	// проверяем аргумент length
+	if length <= 0 {
+		return ""
+	}
+	// проверяем аргумент seed
+	if seed < 0 {
+		seed = -seed
+	}
+	if seed == 0 {
+		seed = 1
+	}
 	current := seed
 	result := ""
 	alphabet := lowercase
+	// создаём набор символов в зависимости от флагов
 	if useUppercase {
 		alphabet += uppercase
 	}
@@ -22,10 +34,12 @@ func GeneratePassword(length, seed int, useUppercase, useDigits, useSpecial bool
 	if useSpecial {
 		alphabet += special
 	}
+	// проходим в цикле и генерируем пароль
 	for range length {
 		current = NextRandom(current)
 		index := current % len(alphabet)
 		result = result + string(alphabet[index])
 	}
+	// возвращаем результат
 	return result
 }
